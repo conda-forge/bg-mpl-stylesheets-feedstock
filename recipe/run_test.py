c872e9b334beb161bd31c65c46db4a22bc263306
Simon Billinge <simon.billinge@gmail.com>
2024-07-16 12:07:50 -0400

    run_tests exit code 0 is success

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -9,5 +9,4 @@ if __name__ == "__main__":
     # Default module to import for determining the test location
 
     exit_code = pytest.main(["-v", "bg_mpl_stylesheets/tests"])
-    assert exit_code == 0, "Tests failed"
-    sys.exit(exit_code)
+    assert exit_code == 0
